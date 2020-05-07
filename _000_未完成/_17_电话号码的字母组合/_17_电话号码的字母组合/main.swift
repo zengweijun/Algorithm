81a1e10d2a1fade4f621e7faefadd57d1b93f170
nius <niuszeng@163.com>
2020-05-07 17:18:28 +0800

    dfs开始

---
+++ _000_未完成/_17_电话号码的字母组合/_17_电话号码的字母组合/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  _17_电话号码的字母组合
+//
+//  Created by nius on 2020/5/7.
+//  Copyright © 2020 nius. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+ 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+
+ 示例:
+ 输入："23"
+ 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+ */
+
+class Solution {
+    func letterCombinations(_ digits: String) -> [String] {
+        
+    }
+}
+

bc0fa5084625a295eb6b1258d152d3894883d1d6
nius <niuszeng@163.com>
2020-06-12 17:08:07 +0800

    对称二叉树

---
+++ _000_准备/链表/Leetcode/src/com/nius/node/TreeNode.java
@@ -0,0 +1,8 @@
+package com.nius.node;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
---
+++ _000_准备/链表/Leetcode/src/done/_101_对称二叉树.java
@@ -0,0 +1,42 @@
+package done;
+
+
+import com.nius.node.TreeNode;
+
+/*
+给定一个二叉树，检查它是否是镜像对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+ 
+
+进阶：
+你可以运用递归和迭代两种方法解决这个问题吗？
+
+链接：https://leetcode-cn.com/problems/symmetric-tree
+ */
+public class _101_对称二叉树 {
+    // 双深度优先搜索算法，一条路是左先深度，
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true; // 已遍历完
+        if (node1 == null || node2 == null || node1.val != node2.val) return false;
+        return isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left);
+    }
+}
